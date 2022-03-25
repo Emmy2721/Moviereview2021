07cd464b9eb14ff755077614092ab6a6bb3bafba
Emmy2721 <96807367+Emmy2721@users.noreply.github.com>
2022-03-25 11:10:40 -0700

    final

---
+++ New.py/urls.py
@@ -0,0 +1,13 @@
+from movie.urls import path, include
+from .views import index, products, productDetail
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('products/',products, name='products'),
+    path('productDetail/<int:id>', productDetail, name='detail'),
+    path('newproduct/', views.newProduct, name='newproduct'),
+    path('loginmessage/', views.loginmessage, name='loginmessage'),
+    path('logoutmessage/', views.logoutmessage, name='logoutmessage'),
+]
+]
+
---
+++ New.py/views.py
@@ -0,0 +1,38 @@
+from movie.shortcuts import render, get_object_or_404
+from.models import Product, TechType, Review
+from movie.urls import revers_lazy
+from .forms import ProductForm
+from movie.contrib.auth.decorators import login_required
+# Create your views here.
+def index(request):
+    return render(request, 'tech/index.html')
+    
+def products(request):
+    products_list=Product.objects.all()
+    return render(request, 'tech/products.html',{'product_list: product_list'})
+def productDetail(request, id):
+    product=get_object_or_404(Product, pk=id)
+    return render(request, 'tech/productdetail.html', {'product' : product})
+
+@login_required
+
+def newProduct(request):
+    form=ProductForm
+
+    if request.method=='POST':
+        form=ProductForm(request.POST)
+        if form.is_valid():
+            post=form.save(commit=True)
+            post.save()
+            form=ProductForm()
+    else:
+        form=ProductForm() 
+    return render(request, 'tech/newproduct.html', {'form': form})
+
+def loginmessage(request):
+    return render(request, 'tech/loginmessage.html')
+
+def logoutmessage(request):
+    return render(request, 'tech/logoutmessage.html')
+
+         
